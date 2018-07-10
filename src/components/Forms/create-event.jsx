81844bc556ebee836de229031423b45eec3b727d
AJ Shannon <anjoshannon@gmail.com>
2018-07-10 02:34:30 -0400

    finished create event form data- can now successfully create and post and event

--- src/components/Forms/create-event.jsx
+++ src/components/Forms/create-event.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Row, Input, Container, Card, } from 'react-materialize';
+// import { connect } from 'react-redux';
 
 
 class CreateEvent extends React.Component {
 
     state = {
         
-        user: '',
-        title: ``,
+        title: "",
+        author: "",
         lat: 0,
         lng: 0,
-        game: '',
-        day: '',
-        time: '',
-        link: ''
+        game: "",
+        day: "",
+        time: "",
+        link: ""
 
       }
 
     titleOnChange = (event) => {
         this.setState({
             title: event.target.value
+        })
+    }
+    authorOnChange = (event) => {
+        this.setState({
+            author: event.target.value
         })
     }
     latOnChange = (event) => {
@@ -52,12 +58,39 @@ class CreateEvent extends React.Component {
             link: event.target.value
         })
     }
-      handleSubmit = (e) => {
+    handleSubmit = (e) => {
         // this.props.dispatch(addUser(this.state));
         console.log(this.state)
-        this.setState({
-           
-        })
+
+        fetch("http://159.65.38.99/events/add-raw",
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // 'Authorization': 'Bearer ' + state.token,
+                    },
+                    mode: "cors",
+                    body: JSON.stringify({
+
+                        title: this.state.title,
+                        author: this.state.author,
+                        lat: this.state.lat,
+                        lng: this.state.lng,
+                        game: this.state.game,
+                        day: this.state.day,
+                        time: this.state.time,
+                        link: this.state.link
+
+                    }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    // alert("This event was posted!")
+                })
+                
+
+
       }
 
     render() {
@@ -66,8 +99,8 @@ class CreateEvent extends React.Component {
                 <Card id="SUPCard" className='large'
                     actions={[ 
                         <div id="LOGSubmit">
-                            <button onClick={ this.handleSubmit } class="btn waves-effect waves-light" type="submit" name="action">Post Event
-                                <i class="material-icons right"></i>
+                            <button onClick={ this.handleSubmit } className="btn waves-effect waves-light" type="submit" name="action">Post Event
+                                <i className="material-icons right"></i>
                             </button>
                         </div>
                     ]}
@@ -78,23 +111,31 @@ class CreateEvent extends React.Component {
                             <Row id="LOGContent">
 
                                 <Input onChange={ this.titleOnChange } value={this.state.title} s={12} label="event title" placeholder="EX: Smash Brothers Friday's Final Destination at Guardian Games" />
-                                <Input  onChange={ this.gameOnChange } id="createGameSelect" s={12} type='select' label='Game' icon='games' defaultValue='1' >
+                                <Input onChange={ this.authorOnChange } value={this.state.author} s={12} label="author" placeholder="EX: Juan-dicimo" />
+                                <Input onChange={ this.gameOnChange } s={12} type='select' label='Game' icon='games'  defaultValue="choose a game">
                                     <option value='none'>choose a game</option>
                                     <option value='Street Fighter 5'>Street Fighter 5</option>
                                     <option value='Super Smash Wii-u'>Super Smash Wii-u</option>
                                     <option value='Magic the Gathering'>Magic the Gathering</option>
                                     <option value='Yu-gi-oh'>Yu-gi-oh</option>
+                                    <option value='Settlers of Catan'>Settlers of Catan</option>
+                                    <option value='Super Smash Bros. Melee'>Super Smash Bros. Melee</option>
                                 </Input>
                                 <Input onChange={ this.latOnChange } value={this.state.lat} type="number" label="latitude" placeholder="39.683099" s={6} />
-                                <Input onChange={ this.lngOnChange } value={this.state.long} type="number" label="longitude" placeholder="-86.148345" s={6} />
-                                <Input onChange={ this.dayOnChange } value={this.state.day} type="text" label="Day of the week" placeholder="EX: Friday" s={6} />
-                                {/* <Input onChange={ this.timeOnChange } value={this.state.time} type="text" label="time" placeholder="EX: 3:00pm" s={6} />
-                                <Input onChange={ this.linkOnChange } value={this.state.link} type="text" label="link" placeholder="EX: https://www.facebook.com/events/1410766155717859/ " s={12} /> */}
+                                <Input onChange={ this.lngOnChange } value={this.state.lng} type="number" label="longitude" placeholder="-86.148345" s={6} />
+                                <Input onChange={ this.dayOnChange } id="daySelect"  type="select" label="Day of the week" defaultValue="Sunday" s={6} >
+                                    <option value='Sunday'>Sunday</option>
+                                    <option value='Monday'>Monday</option>
+                                    <option value='Tuesday'>Tuesday</option>
+                                    <option value='Wednesday'>Wednesday</option>
+                                    <option value='Thursday'>Thursday</option>
+                                    <option value='Friday'>Friday</option>
+                                    <option value='Saturday'>Saturday</option>
+                                </Input>
+                                <Input onChange={ this.timeOnChange } value={this.state.time} type="text" label="Time" placeholder="Ex: 7:00pm" s={6} />
+                                <Input onChange={ this.linkOnChange } value={this.state.link} type="text" label="link" placeholder="http://www.example.com/" s={12} />
 
 
-
-                                {/* <Input name='on' type='time' onChange={function(e, value) {}} />
-                                <Input name='on' type='date' onChange={function(e, value) {}} /> */}
 
                             </Row>
                         </Container>
